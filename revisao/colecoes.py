b138ab3571bc03da499c0efa5cd2b2eef8d81a53
Rogerio Atem Carvalho <ratem@iff.edu.br>
2016-01-28 19:42:21 -0200

    Basico de listas e dicionarios

---
+++ revisao/colecoes.py
@@ -0,0 +1,21 @@
+if __name__ == "__main__":
+	#lista deve declarada antes
+	numeros = []
+	numeros.append(1)
+	numeros.append(2)
+	numeros.append(3)
+	print numeros
+	numeros.append("isso eh horrivel, mas eh possivel!")
+	print numeros
+	print numeros[0]
+	del numeros[3]
+	print numeros
+	#dicionarios devem ser declarados antes
+	coisas = {}
+	coisas["Maria"] = ["couve", 22]
+	coisas[1] = 5
+	print coisas
+	print coisas["Maria"]
+	print coisas.keys()
+	print coisas.values()
+	print coisas.items()
